52563dfe456900f79db151b0d715d84eded5881c
Daniel Ovaska <daniel.ovaska@hotmail.com>
2018-05-15 10:37:44 +0200

    Removing interface

--- EpiserverSite4/Controllers/ITextAnalyticsRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EpiserverSite4.Controllers
-{
-    public interface ITextAnalyticsRepository
-    {
-        string GetLanguage(string text);
-        double? GetSentiment(string text);
-    }
-}
